7af425c6ac8aeac706087d2eb38fff2c22158901
MichealHamoud <micheal.hamoud11@outlook.com>
2023-05-29 20:58:02 +1000

    added passport file

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const routes = require ('./controller');
-const sequelize = require('./config/connection');
-const app= express();
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const routes = require("./controller");
+const sequelize = require("./config/connection");
+const app = express();
+const passport = require("passport");
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+  app.listen(PORT, () => console.log("Now listening"));
+});
