0036584ba77354e22c919ad8a49fe249459a1aa1
S Chopra <sanjaybenu@gmail.com>
2023-05-29 21:08:17 +1000

    Revert "added passport file"
    This reverts commit 7af425c6ac8aeac706087d2eb38fff2c22158901.

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-const express = require("express");
-const path = require("path");
-const exphbs = require("express-handlebars");
-const routes = require("./controller");
-const sequelize = require("./config/connection");
-const app = express();
-const passport = require("passport");
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require ('./controller');
+const sequelize = require('./config/connection');
+const app= express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-});
+    app.listen(PORT, () => console.log('Now listening'));
+  });
