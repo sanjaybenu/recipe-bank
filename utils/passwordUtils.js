8f0240056cd1f1409907d849b153b3ca62efca95
MichealHamoud <micheal.hamoud11@outlook.com>
2023-05-30 21:04:04 +1000

    fixed an error

--- utils/passwordUtils.js
+++ utils/passwordUtils.js
@@ -1,1 +1,11 @@
+const bcrypt = require("crypto");
 
+function genPassword(password) {
+  var password = bcrypt.randomBytes(32).toString("hex");
+
+  return {
+    password: password,
+  };
+}
+
+module.exports = genPassword;
