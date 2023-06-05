1d1a9c903270700e275361705125c7672e540345
stuart teh <stuartteh1995@gmail.com>
2023-06-05 20:12:13 +1000

    debug

--- seeds/seed.js
+++ seeds/seed.js
@@ -21,21 +21,21 @@ const userData = [
 
 const recipeData = [
   {
-    name: 'dish1',
+    name: 'Thali',
     recipe: 'Recipe 1 content',
     cuisine: 'Indian',
     food_img:'https://c4.wallpaperflare.com/wallpaper/869/719/717/cuisine-food-india-indian-wallpaper-preview.jpg',
     user_id: 1
   },
   {
-    name: 'dish2',
+    name: 'Pizza',
     recipe: 'Recipe 2 content',
     cuisine: 'Italian',
     food_img:'https://c4.wallpaperflare.com/wallpaper/234/543/684/food-pizza-wallpaper-preview.jpg',
     user_id: 2
   },
   {
-    name: 'dish 3',
+    name: 'Tacos',
     recipe: 'Recipe 3 content',
     cuisine: 'Mexican',
     food_img:'https://c4.wallpaperflare.com/wallpaper/570/745/92/comida-mexico-plato-tacos-wallpaper-preview.jpg',
@@ -43,29 +43,12 @@ const recipeData = [
   }
 ];
 
-// const commentData = [
-//   {
-//     comment: 'Comment 1',
-//     recipe_id: 1,
-//     user_id: 2
-//   },
-//   {
-//     comment: 'Comment 2',
-//     recipe_id: 1,
-//     user_id: 3
-//   },
-//   {
-//     comment: 'Comment 3',
-//     recipe_id: 3,
-//     user_id: 3
-//   }
-// ];
+
 
 const seed = async () => {
   await sequelize.sync({ force: true });
   await User.bulkCreate(userData);
   await Recipe.bulkCreate(recipeData);
-  // await Comment.bulkCreate(commentData);
 
   process.exit(0);
 };
